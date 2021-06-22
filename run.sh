e08d0dd211d1abb7f2ed6d7d4be33ef48398a7ca
okyrahmanto <oky.rahmanto@gmail.com>
2021-06-22 15:17:29 +0800

    add capablity to run 2 host separately

--- run.sh
+++ run.sh
@@ -17,21 +17,21 @@ fi
 
 if [ "$1" == "deploy-chaincode" ]; then    
     
-    pushd ../chaincode/fabcar/go
+    pushd ../chaincode/trusted-chaincode
     GO111MODULE=on go mod vendor
     popd
     sleep 5
 
     # package
-    docker exec cli peer lifecycle chaincode package fabcar.tar.gz --path github.com/chaincode/fabcar/go --label fabcar_1
+    docker exec cli peer lifecycle chaincode package trusted-chaincode.tar.gz --path github.com/chaincode/trusted-chaincode --label trusted-chaincode_1
     sleep 3
 
     # peer0.org1
-    docker exec cli peer lifecycle chaincode install fabcar.tar.gz
+    docker exec cli peer lifecycle chaincode install trusted-chaincode.tar.gz
     sleep 3
 
     # peer0.org2
-    docker exec -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp -e CORE_PEER_ADDRESS=peer0.org2.example.com:9051 -e CORE_PEER_LOCALMSPID="Org2MSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt cli peer lifecycle chaincode install fabcar.tar.gz
+    docker exec -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp -e CORE_PEER_ADDRESS=peer0.org2.example.com:9051 -e CORE_PEER_LOCALMSPID="Org2MSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt cli peer lifecycle chaincode install trusted-chaincode.tar.gz
     sleep 3
 
 fi 
