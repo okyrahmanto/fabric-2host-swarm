4e704a008451fbef5a8bc77fc86c7fd34bca4372
okyrahmanto <oky.rahmanto@gmail.com>
2021-06-22 15:33:08 +0800

    add sleep for wait commit ready

--- approvechaincode.sh
+++ approvechaincode.sh
@@ -2,14 +2,19 @@
 
 # for org1
 docker exec cli peer lifecycle chaincode approveformyorg --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --channelID mychannel --name trusted-chaincode --version 1 --sequence 1 --waitForEvent --package-id $1
+sleep 3
+
 # for org2
 docker exec -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp -e CORE_PEER_ADDRESS=peer0.org2.example.com:9051 -e CORE_PEER_LOCALMSPID="Org2MSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt cli peer lifecycle chaincode approveformyorg --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --channelID mychannel --name trusted-chaincode --version 1 --sequence 1 --waitForEvent --package-id $1
+sleep 3
 
 #approval status
 docker exec cli peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name trusted-chaincode --version 1 --sequence 1
+sleep 2
 
 #commit chaincode
 docker exec cli peer lifecycle chaincode commit -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt --channelID mychannel --name trusted-chaincode --version 1 --sequence 1
+sleep 3
 
 #commit status
 docker exec cli peer lifecycle chaincode querycommitted --channelID mychannel --name trusted-chaincode
